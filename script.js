36723f9857aebc4a6631e31d3973ff0c7a789fe7
Taiyodayou <97333568+Taiyodayou@users.noreply.github.com>
2022-01-22 17:50:54 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+$(function(){
+
+
+  $('.top-slider').slick({
+    autoplay:true,
+    arrows:false,
+    draggable:false,
+    speed: 800,
+    fade: true,
+    pauseOnFocus:false,
+    pauseOnHover:false,
+    cssEase: 'linear'
+  });
+
+
+});
