bc6067d4d93d5146d1ed03d4d99385ff451c5e05
Taiyodayou <97333568+Taiyodayou@users.noreply.github.com>
2022-01-24 23:28:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -17,25 +17,6 @@ $(function(){
     arrows:NamedNodeMap,
     infinite: true,
     speed: 300,
-    slide$(function(){
-
-
-  $('.top-slider').slick({
-    autoplay:true,
-    arrows:false,
-    draggable:false,
-    speed: 800,
-    fade: true,
-    pauseOnFocus:false,
-    pauseOnHover:false,
-    cssEase: 'linear'
-  });
-
-
-  $('.one-time').slick({
-    arrows:NamedNodeMap,
-    infinite: true,
-    speed: 300,
     slidesToShow: 3,
     swipeToSlide:true,
     adaptiveHeight: true
@@ -49,14 +30,6 @@ $(function(){
     swipeToSlide:true,
     adaptiveHeight: true
   }); 
-
-
-
-});sToShow: 3,
-    swipeToSlide:true,
-    adaptiveHeight: true
-
-  });
 
 
 
